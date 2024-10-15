460918dbff4afe2c454013b7fd705ecd52016212
user706 <39215612+user706@users.noreply.github.com>
2024-10-15 18:25:39 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # pybind11_test
+
+```bash
+mkdir build/
+cd    build/
+cmake ..
+cmake --build . -j    # will also copy mysum.py into the build/ directory
+./go                  # run
+```
+
+The result is:
+
+```
+Sum is: 15
+double free or corruption (out)
+Aborted (core dumped)
+```
---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <pybind11/embed.h> // everything needed for embedding
+#include <pybind11/numpy.h>
+#include <vector>
+#include <thread>
+
+namespace py = pybind11;
+
+
+py::object *my_sum; // lifetime beyond certain scoping
+
+void func()
+{
+    py::gil_scoped_acquire acquire;
+
+    std::vector<int16_t> vec;
+    vec.resize(200);
+    vec[0] = 10;
+    vec[1] = 5;
+    
+    py::array_t<int16_t, py::array::c_style> arr(vec.size(), vec.data());
+    std::cout << "Sum is: " << (*my_sum)(arr).cast<int64_t>() << std::endl;
+}
+
+
+int main() {
+    constexpr int NUM_LOOPS = 2;
+    for (int i = 0; i < NUM_LOOPS; ++i) {
+        py::scoped_interpreter inter;
+        {
+            py::object imported = py::module::import("mysum");
+            my_sum = new py::object(imported.attr("my_sum"));
+        }
+        {
+            py::gil_scoped_release rel;
+            std::thread th1(func);
+            th1.join();
+        }
+        delete my_sum;
+    }
+}
